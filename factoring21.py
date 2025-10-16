0bf987a4543a9b36f3e3ef3c5fd543789e927c92
Tung Bui <tungbuidang@gmail.com>
2025-10-16 13:50:20 +0300

    factoring21 is working now

--- factoring21.py
+++ factoring21.py
@@ -1,11 +1,14 @@
 import qiskit.qasm2 
 import matplotlib 
-
+import math 
+from fractions import Fraction
 from qiskit_aer import AerSimulator
 from qiskit.circuit import QuantumCircuit
 from qiskit.transpiler import generate_preset_pass_manager
 from qiskit_ibm_runtime import Session, SamplerV2 as Sampler
 from qiskit_aer.primitives import SamplerV2
+import pandas as pd 
+
 
 program = """
     OPENQASM 2.0;
@@ -28,15 +31,61 @@ pm = generate_preset_pass_manager(backend=sim, optimization_level=0)
 qc = pm.run(circuit)
 print(qc)
 
-count = sim.run(circuit, shots=100).result().get_counts(circuit)
-# print(count)
+shot_counts = 1000
+count = sim.run(circuit, shots=shot_counts).result().get_counts(circuit)
+
+print(type(count))
+
+
+# organize data 
+number_hit =  [] 
+frequency  =  []
+fraction   =  []
+denominator = []
 
 for measured_value in count:
-   binary_str = ''.join(measured_value[::-1])
-   print(binary_str)
+    num = measured_value[::].replace(' ', '')  # remove spaces
+    value = count.get(measured_value[::])
+    print("value:", value)
+    frequency.append(value)
+    number_hit.append(int(num, 2))  
+    frac = Fraction(int(num, 2),1024).limit_denominator(21)
+    fraction.append(frac)
+    denominator.append(frac.denominator)
 
-from qiskit.visualization import plot_histogram
-plot_histogram(count) 
+df = pd.DataFrame ({
+    "number hit": number_hit,
+    "frequency": frequency,
+    "fraction": fraction,
+    "denominator": denominator
+})
 
-import matplotlib.pyplot as plt
-plt.show()
+print("Original data sampled from the quantum circuit: ")
+print(df)
+filtered_df = df[df['frequency'] > shot_counts*0.04] # equivalent to more than 40 counts for every 1000 shots  
+
+print("Remove some noisy element that have low count: ")
+print(filtered_df)
+
+period = []
+for num in filtered_df['denominator']:
+    if num not in period and num % 2 == 0:
+        period.append(num)
+
+print("possible period value to find the factors: ")
+print(period)
+
+for num in period: 
+    guess1 = 2**(num//2) + 1 
+    guess2 = 2**(num//2) - 1
+    if math.gcd(guess1, 21) not in [1, 21]:
+        print("factor found: ", math.gcd(guess1, 21))
+    if math.gcd(guess2, 21) not in [1, 21]:
+        print("factor found: ", math.gcd(guess2, 21))
+
+
+# from qiskit.visualization import plot_histogram
+# plot_histogram(count) 
+
+# import matplotlib.pyplot as plt
+# plt.show()
